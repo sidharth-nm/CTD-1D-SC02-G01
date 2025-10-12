cfeb0ee2406d5bd2b881860658c1edd575f4b981
sidharth-nm <sidharth.nimmagadda@gmail.com>
2025-10-12 21:54:01 +0800

    importing files from Drive

---
+++ mylibrary.py
@@ -0,0 +1,7 @@
+
+def calculate_total( oranges, apples):
+    return 2*oranges + 3*apples
+
+def plastic_bags_price(bags):
+    """one bag 0.1, two bags 0.4, three bags 0.9 """
+    return 0.1*bags*bags
